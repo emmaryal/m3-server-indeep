7a59c13b5874915a786a97f1f5a4bc15730a8bf3
Emma Craig <emma.ryal@gmail.com>
2020-12-14 18:14:31 +0100

    typo fixed

--- routes/records.router.js
+++ routes/records.router.js
@@ -2,29 +2,51 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-
-const User = require("../models/user.model")
+const User = require("../models/user.model");
 const Record = require("../models/record.model");
 
 //get all records
 router.get("/records", (req, res, next) => {
-    Record.find()
+  Record.find()
     .populate("favoritedBy")
-      .then((allTheRecords) => {
-        res.status(200).json(allTheRecords);
-      })
-      .catch((err) => {
-        next(err)
-      });
-  });
+    .then((allTheRecords) => {
+      res.status(200).json(allTheRecords);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
-  //create new record
-router.post("/records", (req, res, next) =>{
+//create new record
+router.post("/records", (req, res, next) => {
+  const {
+    title,
+    artist,
+    format,
+    image,
+    label,
+    mediaCondition,
+    sleeveCondition,
+    weight,
+    comments,
+    catno,
+    price,
+  } = req.body;
 
-  const {title, artist, format, image, label, mediaCondition, sleeveCondition, weight, comments,catno,price} = req.body;
-    
-  
-Record.create({title, artist, format, image, label, mediaCondition, sleeveCondition, favoritedBy: [], weight, comments,catno,price})
+  Record.create({
+    title,
+    artist,
+    format,
+    image,
+    label,
+    mediaCondition,
+    sleeveCondition,
+    favoritedBy: [],
+    weight,
+    comments,
+    catno,
+    price,
+  })
     .then((createdRecord) => {
       res.status(201).json(createdRecord);
     })
@@ -37,65 +59,112 @@ Record.create({title, artist, format, image, label, mediaCondition, sleeveCondit
 
 //get a record by id
 router.get("/records/:id", (req, res) => {
+  const { id } = req.params;
 
-        const { id } = req.params;
-      
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-          res
-            .status(400) //  Bad Request
-            .json({ message: "Specified id is not valid" });
-          return;
-        }
-      
-        Record.findById(id)
-         
-  .populate("favoritedBy")
-          .then((foundRecord) => {
-            res.status(200).json(foundRecord); // OK
-          })
-          .catch((err) => {
-            res.status(500).json(err); // Internal Server Error
-          });
-      });
-  
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res
+      .status(400) //  Bad Request
+      .json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Record.findById(id)
+
+    .populate("favoritedBy")
+    .then((foundRecord) => {
+      res.status(200).json(foundRecord); // OK
+    })
+    .catch((err) => {
+      res.status(500).json(err); // Internal Server Error
+    });
+});
+
 //update a record
 router.put("/records/:id", (req, res, next) => {
-    const { id } = req.params;
-    const {listingId, artist, title, label, catno,format, releaseId, status, price, listed, comments, mediaCondition, sleeveCondition, acceptOffer, externalId, weight, formatQuantity, flatShipping, location, favoritedBy, image} = req.body;
-  
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      res.status(400).json({ message: "Specified id is not valid" });
-      return;
-    }
-  
-    Record.findByIdAndUpdate(id, {listingId, artist, title, label, catno,format, releaseId, status, price, listed, comments, mediaCondition, sleeveCondition, acceptOffer, externalId, weight, formatQuantity, flatShipping, location, favoritedBy, image}, {new:true})
-      .then((updatedRecord) => {
-        res.status(200).json(updatedRecord)
-      })
-      .catch((err) => {
-        res.status(500).json(err);
-      });
-  });
- 
-//delete a record  
-router.delete('/records/:id', (req, res)=>{
-    const { id } = req.params;
-  
-    if ( !mongoose.Types.ObjectId.isValid(id)) {
-      res.status(400).json({ message: 'Specified id is not valid' });
-      return;
-    }
-  
-    Record.findByIdAndRemove(id)
-      .then(() => {
-        res
-          .status(202)  //  Accepted
-          .send(`Document ${id} was removed successfully.`);
-      })
-      .catch( err => {
-        res.status(500).json(err);
-      })
-  });  
+  const { id } = req.params;
+  const {
+    listingId,
+    artist,
+    title,
+    label,
+    catno,
+    format,
+    releaseId,
+    status,
+    price,
+    listed,
+    comments,
+    mediaCondition,
+    sleeveCondition,
+    acceptOffer,
+    externalId,
+    weight,
+    formatQuantity,
+    flatShipping,
+    location,
+    favoritedBy,
+    image,
+  } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Record.findByIdAndUpdate(
+    id,
+    {
+      listingId,
+      artist,
+      title,
+      label,
+      catno,
+      format,
+      releaseId,
+      status,
+      price,
+      listed,
+      comments,
+      mediaCondition,
+      sleeveCondition,
+      acceptOffer,
+      externalId,
+      weight,
+      formatQuantity,
+      flatShipping,
+      location,
+      favoritedBy,
+      image,
+    },
+    { new: true }
+  )
+    .then((updatedRecord) => {
+      res.status(200).json(updatedRecord);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+//delete a record
+router.delete("/records/:id", (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Record.findByIdAndRemove(id)
+    .then(() => {
+      res
+        .status(202) //  Accepted
+        .send(`Document ${id} was removed successfully.`);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
 //get all users
 /* router.get("/users", (req, res, next) => {
@@ -111,50 +180,48 @@ router.delete('/records/:id', (req, res)=>{
 //update favourites - add users to array
 router.put("/users/:id/records/:recordId", (req, res, next) => {
   const { id, recordId } = req.params;
-  
 
-  if (!mongoose.Types.ObjectId.isValid(id) || !mongoose.Types.ObjectId.isValid(recordId)) {
+  if (
+    !mongoose.Types.ObjectId.isValid(id) ||
+    !mongoose.Types.ObjectId.isValid(recordId)
+  ) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
 
-  Record.findByIdAndUpdate(recordId,
+  Record.findByIdAndUpdate(
+    recordId,
     { $push: { favoritedBy: id } },
-    { new: true })
+    { new: true }
+  )
     .then((updatedRecord) => {
-      
-      res.status(200).json(updatedRecord)
+      res.status(200).json(updatedRecord);
     })
     .catch((err) => {
       res.status(500).json(err);
     });
 });
 
-  //get current user by session id
+//get current user by session id
 router.get("/users/:id", (req, res, next) => {
-  
   console.log("req.params:", req.params);
   const { id } = req.params;
-console.log("id:", id);
-console.log("req.params:", req.params);
+  console.log("id:", id);
+  console.log("req.params:", req.params);
   if (!mongoose.Types.ObjectId.isValid(id)) {
-   res
-     .status(400) //  Bad Request
+    res
+      .status(400) //  Bad Request
       .json({ message: "Specified id is not valid" });
-   return;
+    return;
   }
 
   User.findById(id)
-  
-  .then((foundUser) => {
-    res.status(200).json(foundUser); // OK
-  })
-
+    .then((foundUser) => {
+      res.status(200).json(foundUser); // OK
+    })
     .catch((err) => {
       res.status(500).json(err); // Internal Server Error
     });
 });
-
-
 
 module.exports = router;
